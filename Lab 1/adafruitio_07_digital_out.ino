4ca601a3996e4c6e13a79cc376c937535d5fd5b1
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-18 11:14:05 +0800

    lab 1

---
+++ Lab 1/adafruitio_07_digital_out.ino
@@ -0,0 +1,93 @@
+// Adafruit IO Digital Output Example
+// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-digital-output
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+#include <M5StickCPlus.h>
+/************************ Example Starts Here *******************************/
+
+// digital pin 5
+#define LED_PIN 5
+
+// this function is called whenever an 'digital' feed message
+// is received from Adafruit IO. it was attached to
+// the 'digital' feed in the setup() function above.
+void LED_ctrlmsg(AdafruitIO_Data *data) {
+
+  Serial.print("received <- ");
+
+  if(data->toPinLevel() == HIGH)
+    Serial.println("HIGH");
+  else
+    Serial.println("LOW");
+
+
+  digitalWrite(LED_PIN, !(data->toPinLevel()));
+}
+
+// set up the 'digital' feed
+AdafruitIO_Feed *digital_lab1 = io.feed("lab1_led");
+
+const int buttonA = 37;
+const int buttonB = 39;
+int cur_valueA = 0;
+int cur_valueB = 0;
+
+void setup() {
+  
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  M5.begin();
+  
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // set up a message handler for the 'digital' feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  digital_lab1->onMessage(LED_ctrlmsg);
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+  digital_lab1 ->get();
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+}
